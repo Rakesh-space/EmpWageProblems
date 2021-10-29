727023ffe70441d8d8b00a0fba1dac743ccf8da4
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 16:11:18 +0530

    Adding to Calculate Daily Wage

--- EmpWageProblem/UC1CheckEmpPreAbse.cs
+++ EmpWageProblem/UC1CheckEmpPreAbse.cs
@@ -8,21 +8,37 @@ namespace EmpWageProblem
 {
     class UC1CheckEmpPreAbse
     {
-        static void main(String[] args)
-        {
-            int IS_FULL_TIME = 1;   //here integer type varable assigned to value
-            Random random = new Random(); //here use Random class 
-
-            int empCheck = random.Next(0, 2);  //here pass the possiblilites of random class
-            if (empCheck == IS_FULL_TIME)  //here cheked the employee is full time or not
+       
+            public const int IS_FULL_TIME = 1; //initialize the value of full time employee
+            public const int IS_PART_TIME = 2; //initialize the value of part time employee
+            public const int Emp_Rate_Per_Hour = 20; //initialize the value of emplayoee rate per hours
+            static void Main(string[] args)
             {
-                Console.WriteLine("\n Employee Present..");
+                int emphrs = 0;
+                int empwage = 0;
+                // creating obj of Random Predefine class
+                Random random = new Random();
+                //using Next() to generaret random input out of 0,1
+                int empInput = random.Next(0, 3);
+                if (IS_FULL_TIME == empInput)
+                {
+                    Console.WriteLine("Employee is present");
+                    emphrs = 8;
+                }
+                else if (IS_PART_TIME == empInput)
+                {
+                    Console.WriteLine("Employee is Part Time present");
+                    emphrs = 4;
+                }
+                else
+                {
+                    emphrs = 0;
+                    Console.WriteLine(" Employee is Absent");
+                }
+                empwage = emphrs * Emp_Rate_Per_Hour;
+                Console.WriteLine("empwagec:" + empwage);
+                Console.ReadLine();
             }
-            else
-            {
-                Console.WriteLine("\n Employee is Absent..");
-            }
-            Console.ReadLine();
-        }
     }
+    
 }
