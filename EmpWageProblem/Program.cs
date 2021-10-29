d65bb1ee0bb32c4fb3d3f8d111a04b381d1b46bc
Rakesh-space <rakeshsmusale03@gmail.com>
2021-10-29 13:19:45 +0530

    Adding Welcome message to Employee Wage problem

---
+++ EmpWageProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Emplyoee Wage Problem....");
+        }
+    }
+}
